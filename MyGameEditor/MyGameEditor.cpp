fedd6d80271fde83367324303e78ab5d9e95e5f4
Pep Cots <josep.maria.cots@citm.upc.edu>
2023-10-03 23:51:01 +0200

    OpenGL Immediate Mode set up

--- MyGameEditor/MyGameEditor.cpp
+++ MyGameEditor/MyGameEditor.cpp
@@ -13,26 +13,26 @@
 using namespace std;
 using namespace chrono;
 
-static const int WINDOW_WIDTH = 1280;
-static const int WINDOW_HEIGHT = 720;
-static const int FPS = 60;
+static const unsigned int WINDOW_WIDTH = 576*4/3;
+static const unsigned int WINDOW_HEIGHT = 576;
+static const unsigned int FPS = 60;
+static const auto FDT = 1.0s / FPS;
 
 static SDL_Window* initSDLWindowWithOpenGL() {
-    if (SDL_Init(SDL_INIT_VIDEO)!=0) throw exception(SDL_GetError());
+    if(SDL_Init(SDL_INIT_VIDEO)!=0) throw exception(SDL_GetError());
     
     SDL_version compiled;
     SDL_VERSION(&compiled);
-    cout << "SDL Compiled with " << std::to_string(compiled.major) << '.' << std::to_string(compiled.minor) << '.' << std::to_string(compiled.patch); 
+    cout << "SDL Compiled with " << to_string(compiled.major) << '.' << to_string(compiled.minor) << '.' << to_string(compiled.patch);
 
     SDL_version linked;
     SDL_GetVersion(&linked);
-    cout << "SDL Linked with "  << std::to_string(linked.major) << '.' << std::to_string(linked.minor) << '.' << std::to_string(linked.patch);
+    cout << "SDL Linked with "  << to_string(linked.major) << '.' << to_string(linked.minor) << '.' << to_string(linked.patch);
 
     // setup SDL window
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-
     SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
     // GL 3.1 + GLSL 130
@@ -48,20 +48,16 @@ static SDL_Window* initSDLWindowWithOpenGL() {
 
 static SDL_GLContext createSdlGlContext(SDL_Window* window) {
     auto gl_context = SDL_GL_CreateContext(window);
-    if (!gl_context) throw exception(SDL_GetError());
-
-    if (SDL_GL_MakeCurrent(window, gl_context) != 0) throw exception(SDL_GetError());
-
-    if (SDL_GL_SetSwapInterval(1) != 0) throw exception(SDL_GetError());
-
+    if(!gl_context) throw exception(SDL_GetError());
+    if(SDL_GL_MakeCurrent(window,gl_context)!=0) throw exception(SDL_GetError());
+    if(SDL_GL_SetSwapInterval(1)!=0) throw exception(SDL_GetError());
     return gl_context;
 }
 
 static void initOpenGL() {
     auto glew_init_error = glewInit();
-    if (glew_init_error != GLEW_OK) throw exception((char*)glewGetErrorString(glew_init_error));
-    if (!GLEW_VERSION_3_1) throw exception("OpenGL 3.1 Not Supported!");
-
+    if(glew_init_error!=GLEW_OK) throw exception((char*)glewGetErrorString(glew_init_error));
+    if(!GLEW_VERSION_3_1) throw exception("OpenGL 3.1 Not Supported!");
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     glClearColor(1, 1, 1, 1);
 }
@@ -82,43 +78,42 @@ static bool processSDLEvents() {
     return true;
 }
 
-static void render() {
-    
-    
-   
-}
-
-int main(int argc, char **argv)
+int main(int argc, char* argv[])
 {
     try {
         auto window = initSDLWindowWithOpenGL();
         auto gl_context = createSdlGlContext(window);
         initOpenGL();
 
-        auto t0 = high_resolution_clock::now();
-        auto t1 = high_resolution_clock::now();
-        auto dt = 1000ms / FPS;
-
         MyGameEngine engine;
-        while (processSDLEvents()) {
-            auto t0 = high_resolution_clock::now();
-            engine.step(dt.count()/1000.0);
+        engine.camera.fov = 60;
+        engine.camera.aspect = static_cast<double>(WINDOW_WIDTH) / WINDOW_HEIGHT;
+        engine.camera.zNear = 0.1;
+        engine.camera.zFar = 100;
+        engine.camera.eye = vec3(5, 1.75, 5);
+        engine.camera.center = vec3(0, 1, 0);
+        engine.camera.up = vec3(0, 1, 0);
 
+
+        while (processSDLEvents()) {
+            const auto frame_start = steady_clock::now();
+            engine.step(FDT);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-            engine.render();
+            engine.render(MyGameEngine::RenderModes::DEBUG);
             SDL_GL_SwapWindow(window);
-            auto t1 = high_resolution_clock::now();
-            auto frame_duration = t1 - t0;
-            if(frame_duration<dt) this_thread::sleep_for(dt-frame_duration);
+            const auto frame_end = steady_clock::now();
+            const auto frame_duration = frame_end - frame_start;
+            if (frame_duration < FDT) this_thread::sleep_for(FDT - frame_duration);
         }
 
         SDL_GL_DeleteContext(gl_context);
         SDL_DestroyWindow(window);
         SDL_Quit();
-    }
-    catch (const exception& ex) {
-        cerr << ex.what() << endl;
-    }
 
-    return 0;
+        return EXIT_SUCCESS;
+    }
+    catch(const exception& ex) {
+        cerr << ex.what() << endl;
+        return EXIT_FAILURE;
+    }
 }
--- MyGameEngine/MyGameEngine.h
+++ MyGameEngine/MyGameEngine.h
@@ -1,9 +1,19 @@
 #pragma once
 
+#include <chrono>
+
+#include "types.h"
+#include "Camera.h"
+
 class MyGameEngine
 {
 public:
-	void step(double dt);
-	void render();
+
+	enum class RenderModes {RELEASE, DEBUG};
+
+	Camera camera;
+
+	void step(std::chrono::duration<double> dt);
+	void render(RenderModes renderMode);
 };
 
