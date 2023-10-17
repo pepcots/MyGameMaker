53a2d54511f6a03409478a995761079d7e714727
Pep Cots <josep.maria.cots@citm.upc.edu>
2023-10-17 14:15:12 +0200

    * basyc types updated with vec2 (for texturing prouposes) and single precision vectors (so we can work nativelly with assimp)

--- MyGameEngine/types.h
+++ MyGameEngine/types.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
 
+typedef glm::dvec2 vec2;
 typedef glm::dvec3 vec3;
 typedef glm::dvec4 vec4;
 typedef glm::dmat4 mat4;
+
+
+typedef glm::vec2 vec2f;
+typedef glm::vec3 vec3f;
+typedef glm::vec4 vec4f;
+
